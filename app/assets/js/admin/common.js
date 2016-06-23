705cf7b9aeb2087297ba9b98755ca01dce075c67
王轶 <a@wyr.me>
2016-06-23 12:52:57 +0800

    修复菜单样式

--- app/assets/js/admin/common.js
+++ app/assets/js/admin/common.js
@@ -21,20 +21,10 @@ var nav = responsiveNav(".nav-collapse", { // Selector
 var menu = function () {
     var tmp = 1;
     var $last = $('.active');
-    $(".active > .son").show();
-    $('.nav-collapse > ul > li').click(function () {
-        if($(this).attr("class") != "active"){
-            $('.nav-collapse > ul > li').removeClass('active');
-            $(this).addClass("active");
-
-            $last.children("ul").hide();
-            $last.find('.icon-right i').attr('class','fa fa-angle-left');
-            $(this).find('.icon-right i').attr('class','fa fa-angle-down');
-            $(this).children("ul").show(200);
-            $last = $(this);
-            tmp = 1;
-        }else{
-            var $tmp = $(this);
+    $(".active > ul").show();
+    $('.nav-collapse > ul > li > a').click(function () {
+        var $tmp = $(this).parent();
+        if($tmp.attr("class") == "active"){
             if(tmp == 1){
                 $tmp.children("ul").hide(200,function () {
                     $tmp.find('.icon-right i').attr('class','fa fa-angle-left');
@@ -45,11 +35,75 @@ var menu = function () {
                 $tmp.children("ul").show(200);
                 tmp = 1;
             }
+        }else{
+            $last.removeClass('active');
+            $tmp.addClass("active");
+
+            $last.children("ul").hide();
+            $last.find('.icon-right i').attr('class','fa fa-angle-left');
+            $tmp.find('.icon-right i').attr('class','fa fa-angle-down');
+            $tmp.children("ul").show(200);
+            $last = $tmp;
+            tmp = 1;
+        }
+    });
+
+    //菜单隐藏
+    var tmp2 = 1;
+    $(".admin-top-bars").click(function () {
+        if(tmp2 == 1){
+            $("#nav").hide();
+            $(".main").css("width","100%");
+            tmp2 = 0;
+        }else{
+            $("#nav").show(200);
+            $(".main").css("width","76%");
+            tmp2 = 1;
         }
     });
 };
 
+
+
 $('document').ready(function () {
     menu();
-    
+
+    //路由
+    var showAuthorInfo = function () { console.log("showAuthorInfo"); };
+    var listBooks = function () { console.log("listBooks"); };
+
+    var allroutes = function() {
+        var route = window.location.hash.slice(2);
+        var sections = $('section');
+        var section;
+
+        section = sections.filter('[data-route=' + route + ']');
+
+        if (section.length) {
+            sections.hide(250);
+            section.show(250);
+        }
+    };
+
+    //
+    // define the routing table.
+    //
+    var routes = {
+        '/author': showAuthorInfo,
+        '/books': listBooks
+    };
+
+    //
+    // instantiate the router.
+    //
+    var router = Router(routes);
+
+    //
+    // a global configuration setting.
+    //
+    router.configure({
+        on: allroutes
+    });
+
+    router.init();
 });
