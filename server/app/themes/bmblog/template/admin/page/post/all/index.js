39461fd94ff2b2c5f5234074ae4939b0f1d3d706
王轶 <a@wyr.me>
2016-07-07 22:55:45 +0800

    更新bootstrap版本

--- server/app/themes/bmblog/template/admin/page/post/all/index.js
+++ server/app/themes/bmblog/template/admin/page/post/all/index.js
@@ -16,65 +16,103 @@ Date.prototype.Format = function (fmt) { //author: meizz
     return fmt;
 };
 
-$.ajax({
-    cache: false,
-    type: 'POST',
-    url: "/api/blog/posts",
-    async: true,
-    data: {
-        api_get: 'post'
-    },
-    dataType: "json",
-    success: function (result) {
-        if (result.err == 500) {
-            $("#content-main").html('<h1>数据错误</h1>');
-        } else {
-            $(".all_post").text(result.posts_all);
-            $(".public_post").text(result.posts_public_all);
-            $(".all_page").text(result.posts_page_all);
-            $(".current-page").val(result.posts_now).css('width', result.posts_now.length * 6.75 + 10);
-            var title_max_width = parseInt($('.main').css('width')) * 0.45 + 'px';
-            var body = '';
-            for (var a in result.posts) {
-                if (result.posts[a].post_category == null) {
-                    result.posts[a].post_category = '-';
-                } else {
-                    result.posts[a].post_category = result.posts[a].post_category.split(',');
-                    for (var b in result.posts[a].post_category) {
-                        result.posts[a].post_category[b] = '<a href="/' + result.posts[a].post_category[b] + '" target="_blank">' + result.posts[a].post_category[b] + '</a>';
+var finish_load = function () {
+    var date2 = new Date();
+    $('#e_time').text(date2.getTime() - date1.getTime());
+    $('#r_time').text(new Date().Format("yyyy-MM-dd hh:mm:ss"));
+};
+
+var now_page,all_pages;//全局变量,当前页面/总页面
+
+var get_posts = function (post_page) {
+    $.ajax({
+        cache: false,
+        type: 'POST',
+        url: "/api/blog/posts",
+        async: true,
+        data: {
+            api_get: 'post',
+            post_page: post_page
+        },
+        dataType: "json",
+        success: function (result) {
+            if (result.err == 500) {
+                $("#content-main").html('<h1>数据错误</h1>');
+            } else {
+                all_pages = result.posts_all;
+                now_page = result.posts_now;
+                $(".all_post").text(all_pages);
+                $(".public_post").text(result.posts_public_all);
+                $(".all_page").text(result.posts_page_all);
+                $(".current-page").val(now_page).css('width', now_page.length * 6.75 + 10);
+                var title_max_width = parseInt($('.main').css('width')) * 0.45 + 'px';
+                var body = '';
+                for (var a in result.posts) {
+                    if (result.posts[a].post_category == null) {
+                        result.posts[a].post_category = '-';
+                    } else {
+                        result.posts[a].post_category = result.posts[a].post_category.split(',');
+                        for (var b in result.posts[a].post_category) {
+                            result.posts[a].post_category[b] = '<a href="/' + result.posts[a].post_category[b] + '" target="_blank">' + result.posts[a].post_category[b] + '</a>';
+                        }
                     }
-                }
-                if (result.posts[a].post_tag == null) {
-                    result.posts[a].post_tag = '-';
-                } else {
-                    result.posts[a].post_tag = result.posts[a].post_tag.split(',');
-                    for (var c in result.posts[a].post_tag) {
-                        result.posts[a].post_tag[c] = '<a href="/tag/' + result.posts[a].post_tag[c] + '" target="_blank">' + result.posts[a].post_tag[c] + '</a>';
+                    if (result.posts[a].post_tag == null) {
+                        result.posts[a].post_tag = '-';
+                    } else {
+                        result.posts[a].post_tag = result.posts[a].post_tag.split(',');
+                        for (var c in result.posts[a].post_tag) {
+                            result.posts[a].post_tag[c] = '<a href="/tag/' + result.posts[a].post_tag[c] + '" target="_blank">' + result.posts[a].post_tag[c] + '</a>';
+                        }
                     }
+                    body += '<tr>' +
+                        '<td><input type="checkbox"></td>' +
+                        '<td class="post-td"><a href="#/edit/post/' + result.posts[a].ID + '" target="_blank"><div class="post_title" style="max-width:' + title_max_width + '">' + result.posts[a].post_title + '</div></a></br><div class="post-control">编辑 | 快速编辑 | 移至回收站 | 查看</div></td>' +
+                        '<td><a href="#/edit/post/' + result.posts[a].ID + '" target="_blank">' + result.posts[a].display_name + '</a></td>' +
+                        '<td>' + result.posts[a].post_category + '</td>' +
+                        '<td>' + result.posts[a].post_tag + '</td>' +
+                        '<td style="text-align: center">' + result.posts[a].comment_count + '</td>' +
+                        '<td>已发布</br>' + result.posts[a].post_date + '</td>' +
+                        '</tr>';
                 }
-                body += '<tr>' +
-                    '<td><input type="checkbox"></td>' +
-                    '<td class="post-td"><a href="#/edit/post/' + result.posts[a].ID + '" target="_blank"><div class="post_title" style="max-width:' + title_max_width + '">' + result.posts[a].post_title + '</div></a></br><div class="post-control">编辑 | 快速编辑 | 移至回收站 | 查看</div></td>' +
-                    '<td>' + result.posts[a].display_name + '</td>' +
-                    '<td>' + result.posts[a].post_category + '</td>' +
-                    '<td>' + result.posts[a].post_tag + '</td>' +
-                    '<td style="text-align: center">' + result.posts[a].comment_count + '</td>' +
-                    '<td>已发布</br>' + result.posts[a].post_date + '</td>' +
-                    '</tr>';
+                $("tbody").html(body);
+                finish_load();
             }
-            $("tbody").html(body);
-            var date2 = new Date();
-            $('#e_time').text(date2.getTime() - date1.getTime());
-            $('#r_time').text(new Date().Format("yyyy-MM-dd hh:mm:ss"));
+        },
+        error: function (err) {
+            $("#content-main").html('<h1>' + err.responseText + '(' + err.status + ')' + '</h1>');
+            console.log(err);
         }
-    },
-    error: function (err) {
-        $("#content-main").html('<h1>' + err.responseText + '(' + err.status + ')' + '</h1>');
-        console.log(err);
-    }
-});
+    });
+};
+
+get_posts(1);
 
 $(".current-page").keydown(function () {
     $(this).css('width', ($(this).val().length * 6.75 + 10));
 });
+
+$('.first-page').click(function () {
+    if(now_page == '1'){
+
+    }else{
+        get_posts(1);
+    }
+});
+
+$('.last-page').click(function () {
+    get_posts(1);
+});
+
+$('.next-page').click(function () {
+    get_posts(1);
+});
+
+$('.final-page').click(function () {
+    if(all_pages == now_page){
+
+    }else{
+        get_posts(all_pages);
+    }
+});
+
 
