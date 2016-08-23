26f044e8fe158d9a8297f4de58eb99f82db2352a
wy373226722 <a@12345679.cn>
2016-08-23 12:46:57 +0800

    add

--- app/assets/js/admin/login.js
+++ app/assets/js/admin/login.js
@@ -1,13 +1,4 @@
 $('document').ready(function () {
-
-    console.log(detector.browser.name);
-    if (window.applicationCache) {
-        //alert("你的浏览器支持HTML5");
-    } else {
-        alert("你的浏览器不支持HTML5");
-    }
-
-
     var login = function () {
         $.ajax({
             type: 'POST',
@@ -44,4 +35,60 @@ $('document').ready(function () {
             login();
         }
     });
+
+    console.time("check");
+
+    $("#browser").text(detector.browser.name + " " + detector.browser.fullVersion);
+    $("#engine").text(detector.engine.name + " " + detector.browser.fullVersion);
+    $("#engine_mod").text(detector.engine.mode + " " + detector.engine.fullMode);
+    $("#os").text(detector.os.name + " " + detector.os.fullVersion);
+    $("#device").text(detector.device.name + " " + detector.device.fullVersion);
+    // 判断老旧浏览器
+    if(detector.browser.ie && detector.browser.version < 9){
+        $("#info").html('<div class="alert alert-danger" role="alert">你的浏览器太老了。管理系统暂不支持IE9以下浏览器。请升级浏览器或使用Chrome浏览器。</div>');
+    }
+
+    if (Modernizr.applicationcache) {
+        $("#applicationcache").html('<i class="glyphicon glyphicon-ok"></i>');
+    } else {
+        $("#applicationcache").html('<i class="glyphicon glyphicon-remove"></i>');
+    }
+
+    if (Modernizr.localstorage) {
+        $("#localstorage").html('<i class="glyphicon glyphicon-ok"></i>');
+    } else {
+        $("#localstorage").html('<i class="glyphicon glyphicon-remove"></i>');
+    }
+
+    if (Modernizr.sessionstorage) {
+        $("#sessionstorage").html('<i class="glyphicon glyphicon-ok"></i>');
+    } else {
+        $("#sessionstorage").html('<i class="glyphicon glyphicon-remove"></i>');
+    }
+
+    if (Modernizr.json) {
+        $("#json").html('<i class="glyphicon glyphicon-ok"></i>');
+    } else {
+        $("#json").html('<i class="glyphicon glyphicon-remove"></i>');
+    }
+
+    if (Modernizr.canvas) {
+        $("#canvas").html('<i class="glyphicon glyphicon-ok"></i>');
+    } else {
+        $("#canvas").html('<i class="glyphicon glyphicon-remove"></i>');
+    }
+
+    if (Modernizr.rgba) {
+        $("#rgba").html('<i class="glyphicon glyphicon-ok"></i>');
+    } else {
+        $("#rgba").html('<i class="glyphicon glyphicon-remove"></i>');
+    }
+
+    if (Modernizr.supports) {
+        $("#supports").html('<i class="glyphicon glyphicon-ok"></i>');
+    } else {
+        $("#supports").html('<i class="glyphicon glyphicon-remove"></i>');
+    }
+
+    console.timeEnd("check");
 });
