927a5e7996c724f2f2161b2ab8184fbd1bc763b3
wy373226722 <a@12345679.cn>
2016-09-21 08:43:43 +0800

    change

--- server/api/controllers/index.js
+++ server/api/controllers/index.js
@@ -1,7 +1,2 @@
-// const requireDir = require('require-dir';
-// var dir = requireDir('./');
-//
-// export default dir;
-
 let requireDirectory = require('require-directory');
 module.exports = requireDirectory(module);
